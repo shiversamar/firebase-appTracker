a9d629a129b29a9df4b4fc2e793ed2ddd3d2f5b7
Amar Shivers <shiversamar@yahoo.com>
2017-08-30 16:03:44 -0700

    added pages

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:  {
+      apiKey: 'AIzaSyC5SXVSTASSAvR-At8AvlBI8CZmGPbBeWA',
+      authDomain: 'fir-apptracker.firebaseapp.com',
+      databaseURL: 'https://fir-apptracker.firebaseio.com',
+      projectId: 'fir-apptracker',
+      storageBucket: 'fir-apptracker.appspot.com',
+      messagingSenderId: '45288074425'
+    }
 };
